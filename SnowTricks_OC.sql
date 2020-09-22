ed50f499625d9eddb0772479a40b72082d5dd3de
“Hichamzrk” <hichamzarrouk11047@gmail.com>
2020-09-22 09:53:10 +0200

    update(snowtrick): sql

--- SnowTricks_OC.sql
+++ SnowTricks_OC.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 21, 2020 at 08:45 AM
+-- Generation Time: Sep 22, 2020 at 07:52 AM
 -- Server version: 8.0.21
 -- PHP Version: 7.4.9
 
@@ -38,12 +38,14 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `title`) VALUES
-(7, 'Les dragons'),
-(8, 'Les dragons'),
-(10, 'Grabs'),
 (12, 'Rotations'),
 (14, 'Flips'),
-(16, 'Rotations désaxées');
+(16, 'Rotations désaxées'),
+(17, 'Big air'),
+(18, 'Hip'),
+(19, 'Step-up'),
+(20, 'Half-pipe'),
+(21, 'Quarter-pipe');
 
 -- --------------------------------------------------------
 
@@ -58,14 +60,6 @@ CREATE TABLE `comment` (
   `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`id`, `trick_id_id`, `user_id_id`, `content`, `created_at`) VALUES
-(14, 98, 21, 'Super trick', '2020-09-21 07:40:59'),
-(15, 99, 23, 'Super tricks', '2020-09-21 08:39:01');
 
 -- --------------------------------------------------------
 
@@ -90,7 +84,8 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 ('DoctrineMigrations\\Version20200908142327', '2020-09-08 14:23:31', 169),
 ('DoctrineMigrations\\Version20200909065045', '2020-09-09 06:50:52', 248),
 ('DoctrineMigrations\\Version20200909065836', '2020-09-09 06:58:41', 285),
-('DoctrineMigrations\\Version20200920151424', '2020-09-20 15:14:31', 646);
+('DoctrineMigrations\\Version20200920151424', '2020-09-20 15:14:31', 646),
+('DoctrineMigrations\\Version20200921140903', '2020-09-21 14:09:10', 540);
 
 -- --------------------------------------------------------
 
@@ -109,10 +104,29 @@ CREATE TABLE `image` (
 --
 
 INSERT INTO `image` (`id`, `trick_id_id`, `name`) VALUES
-(177, 98, 'f70b8d7d05d6f718f5df5a1f5646d4e1.jpeg'),
-(178, 99, '332c67ea1e1a65ba9989a0b9904f38bf.jpeg'),
-(179, 99, 'c3a90ec9f1551b8db5bae9efef55bb02.jpeg'),
-(180, 99, '50b1d3a5eff580df49d3244ac04f3dae.jpeg');
+(191, 132, '9f54b4b748f5d37f032b2a5de36782fa.jpeg'),
+(192, 132, 'd5aaa68fad82aacf29882ce77e0231c2.jpeg'),
+(193, 133, 'b4b8839c279713c8dc2c7dc9d146a580.jpeg'),
+(194, 133, '6605ee391ded4a6f82ed4688b6a396e9.jpeg'),
+(195, 133, '07d6132a58710485097a8131c92f0fae.jpeg'),
+(196, 134, 'dd0c90b0707373727a7b2b0d36e3595d.jpeg'),
+(197, 134, '8cdeffe6f6a899487887b31b21771a8d.jpeg'),
+(198, 134, '79496ae8171fb70e81826ba26ad7e78a.jpeg'),
+(199, 135, '95102a338f4351ab77898e253e8de92f.jpeg'),
+(200, 135, 'bb0063b4907e60333423ec32593d2a56.jpeg'),
+(201, 135, '675a0124fc0f8d6af39bbd72e9023d23.jpeg'),
+(202, 136, 'd2430ae5644343cd3a5aa7aa5b5f845f.jpeg'),
+(203, 136, '4c601a23631fa14568a8128bd3dd8c6b.jpeg'),
+(204, 136, 'ac5871deeca5ee94dc6d1579d9942fc1.jpeg'),
+(205, 137, '659c3a5f29989c2cc3ef3d647fd7b41f.jpeg'),
+(206, 137, 'c86c42d3b357799dbd1810e27bcda7e1.jpeg'),
+(207, 137, 'c01b5d0c99bac2c1e7d08545f5783b0f.jpeg'),
+(208, 138, '49dcaa15007b877caf058f2226b8cb57.jpeg'),
+(209, 138, 'b5b0711e1a966e84194fd491c5a4311c.jpeg'),
+(210, 138, '27d68debf8d161822716dbfcc073e78c.jpeg'),
+(211, 139, '228af235b820df6b02c70ff6f7a0b029.jpeg'),
+(212, 139, '0b55c8130316c0e3ab7c68271e5cebe8.jpeg'),
+(213, 139, '1d4fdcd82e747412a9d3322b5c29e1c1.jpeg');
 
 -- --------------------------------------------------------
 
@@ -151,8 +165,14 @@ CREATE TABLE `tricks` (
 --
 
 INSERT INTO `tricks` (`id`, `user_id_id`, `name`, `description`, `image`, `created_at`, `updated_at`, `category_id_id`) VALUES
-(98, 21, 'Le tricks', 'Une astuce fondamentale réalisée en saisissant le bord de pincement entre les liaisons avec la main arrière. Cette astuce est appelée une ponction frontside sur un air droit, ou tout en effectuant une rotation frontside. Lors d\'une rotation d\' antenne arrière ou face arrière, cette ponction est désigné comme un Indy. L\'frontside air a été popularisé par skateur Tony Alva .', 'default-trick.jpg', '2020-09-21 07:39:38', '2020-09-21 07:39:38', 8),
-(99, 23, 'Lien air', 'Lors de l\'exécution d\'un air frontal lors de la transition, le surfeur saisit heelside devant ou derrière le premier plan de liaison avec leur meilleur main. Pour que ce soit un air Lien, le conseil ne peut pas être modifié et doit être maintenu à plat. L\'origine du nom de l\'affaire est l\'orthographe inverse du prénom de skateboarder Neil Blender.', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-phpjz499e-5f68668454e05.jpeg', '2020-09-21 08:37:53', '2020-09-21 08:38:28', 12);
+(132, 21, 'mute', 'saisie de la carre frontside de la planche entre les deux pieds avec la main avant ;', 'default-trick.jpg', '2020-09-22 07:38:02', NULL, 17),
+(133, 21, 'style week', 'saisie de la carre backside de la planche, entre les deux pieds, avec la main avant ;', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-php5CRTNb-5f69aa32da322.jpeg', '2020-09-22 07:39:30', NULL, 12),
+(134, 21, 'indy', 'saisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière ;', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-php4ncYGv-5f69aa61eaf44.jpeg', '2020-09-22 07:40:07', '2020-09-22 07:40:17', 19),
+(135, 21, 'tail grab', 'saisie de la partie arrière de la planche, avec la main arrière ;', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-phpqqPD7Q-5f69aa9061e39.jpeg', '2020-09-22 07:41:04', NULL, 16),
+(136, 21, 'nose grab', 'saisie de la partie avant de la planche, avec la main avant ;', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-phpiPg73Y-5f69aabda24ba.jpeg', '2020-09-22 07:41:49', NULL, 12),
+(137, 21, 'japan', 'saisie de l\'avant de la planche, avec la main avant, du côté de la carre frontside.', 'default-trick.jpg', '2020-09-22 07:42:24', NULL, 21),
+(138, 21, 'seat belt:', 'saisie du carre frontside à l\'arrière avec la main avant ;', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-phpLrYZG5-5f69ab067ff21.jpeg', '2020-09-22 07:43:02', NULL, 12),
+(139, 21, 'truck driver', 'saisie du carre avant et carre arrière avec chaque main (comme tenir un volant de voiture)', 'private-var-folders-ll-q2ltd0n94t955j6p1g2207v00000gn-T-phpy89Nxk-5f69ab445add5.jpeg', '2020-09-22 07:44:04', NULL, 12);
 
 -- --------------------------------------------------------
 
@@ -194,8 +214,14 @@ CREATE TABLE `video` (
 --
 
 INSERT INTO `video` (`id`, `trick_id_id`, `url`) VALUES
-(120, 98, 'https://www.youtube.com/embed/vf9Z05XY79A'),
-(121, 99, 'https://www.youtube.com/embed/f9FjhCt_w2U');
+(152, 132, 'https://www.youtube.com/embed/0Oez89EoE_c'),
+(153, 133, 'https://www.youtube.com/embed/vquZvxGMJT0'),
+(154, 134, 'https://www.youtube.com/embed/SlhGVnFPTDE'),
+(155, 135, 'https://www.youtube.com/embed/c6ry31Wc8sI'),
+(156, 136, 'https://www.youtube.com/embed/xhvqu2XBvI0'),
+(157, 137, 'https://www.youtube.com/embed/eGJ8keB1-JM'),
+(158, 138, 'https://www.youtube.com/embed/FMHiSF0rHF8'),
+(159, 139, 'https://www.youtube.com/embed/vf9Z05XY79A');
 
 --
 -- Indexes for dumped tables
@@ -240,6 +266,7 @@ ALTER TABLE `reset_password_request`
 --
 ALTER TABLE `tricks`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
   ADD KEY `IDX_E1D902C19D86650F` (`user_id_id`),
   ADD KEY `UNIQ_E1D902C19777D11E` (`category_id_id`) USING BTREE;
 
@@ -265,19 +292,19 @@ ALTER TABLE `video`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `image`
 --
 ALTER TABLE `image`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=214;
 
 --
 -- AUTO_INCREMENT for table `reset_password_request`
@@ -289,19 +316,19 @@ ALTER TABLE `reset_password_request`
 -- AUTO_INCREMENT for table `tricks`
 --
 ALTER TABLE `tricks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
 
 --
 -- Constraints for dumped tables
